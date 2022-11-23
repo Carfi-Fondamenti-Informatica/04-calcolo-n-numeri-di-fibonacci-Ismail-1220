a9da892756e741649d6aad84294cc245adb7f72a
Ismail-1220 <118005408+Ismail-1220@users.noreply.github.com>
2022-11-23 12:37:04 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,21 +6,17 @@ int main() {
     int n=0;
     int fibonacci_p= 1, fibonacci=1;
     cin >> n;
-    if(n>=2) 
-    {
+    if(n>=2) {
         cout << fibonacci << endl;
         cout << fibonacci_p << endl;
-        for (int i = 1; i < n - 1; i++)
-        {
+        for (int i = 1; i < n - 1; i++) {
             int temp = fibonacci;
             fibonacci += fibonacci_p;
             fibonacci_p = temp;
             cout << fibonacci << endl;
         }
-    }
-    else
-    {
+    }else{
         cout << "errore"<<endl;
     }
-    return (0);
+    return 0;
 }
